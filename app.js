ea715a655ad9eea3d88d37d4163a9ec72afcd9c3
usa3amustafa <usa3a3ustafa@outlook.com>
2022-09-13 19:40:08 +0500

    homepage

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-const preLoader = document.querySelector('.preloader')
-const nav = document.querySelector('nav')
-const mainLogo = document.querySelector('.main-logo')
-const tagLine = document.querySelector('.tagline')
-const scrollDownImg = document.querySelector('.scroll-down-img')
-
 // slideshow
 $('header').vegas({
   delay: 7000,
@@ -11,38 +5,47 @@ $('header').vegas({
   loop: true,
   shuffle: false,
   firstTransition: 'fade',
-  firstTransitionDuration: 5000,
+  firstTransitionDuration: 1000,
   transition: 'fade',
   transitionDuration: 2000,
   slides: [
-    { src: './img/header/NYC_Image1.png' },
-    { src: './img/header/ATL_Image2.png' },
-    { src: './img/header/LA_Image3.png' },
+    { src: './img/header/NYC_Image1.webp' },
+    { src: './img/header/ATL_Image2.webp' },
+    { src: './img/header/LA_Image3.webp' },
   ],
 })
 
-const tagline = document.querySelector('.tagline')
+const preLoader = document.querySelector('#preloader')
+const nav = document.querySelector('nav')
+const mainLogo = document.querySelector('.main-logo')
+const tagLine = document.querySelector('.tagline')
+const scrollDownImg = document.querySelector('.scroll-down-img')
 
 // type writing
-const typewriter = new Typewriter(tagline, {
+const typewriter = new Typewriter(tagLine, {
   loop: true,
-  delay: 75,
+  delay: 100,
 })
-
-typewriter
-  .pauseFor(4500)
-  .typeString('creative director')
-  .pauseFor(1000)
-  .deleteAll()
-  .typeString('creative director')
-  .pauseFor(1000)
-  .start()
 
 window.addEventListener('load', function () {
   preLoader.style.display = 'none'
 
   nav.classList.add('slidedown')
   mainLogo.classList.add('slideup')
-  tagline.classList.add('fadein')
+  tagLine.classList.add('fadein-animation')
   scrollDownImg.classList.add('fade-in-down-animation')
+
+  typewriter
+    .pauseFor(1300)
+    .typeString('creative director')
+    .pauseFor(1000)
+    .deleteChars(8)
+    .pauseFor(500)
+    .typeString('designer')
+    .pauseFor(1000)
+    .deleteChars(8)
+    .pauseFor(500)
+    .typeString('illustrator')
+    .pauseFor(1000)
+    .start()
 })
